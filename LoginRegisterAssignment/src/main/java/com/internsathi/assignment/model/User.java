5a3872e72b2e23e556a20ce3c37434ab4156a1cd
prabin.sc1 <prabin.sc1@gmail.com>
2023-07-20 21:10:02 +0545

    hibernate validator dependency added and applied on model class

--- LoginRegisterAssignment/src/main/java/com/internsathi/assignment/model/User.java
+++ LoginRegisterAssignment/src/main/java/com/internsathi/assignment/model/User.java
@@ -1,9 +1,14 @@
 package com.internsathi.assignment.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -14,13 +19,20 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 
+	@NotBlank(message = "Name shouldn't be blank")
+	@Size(min = 2, max = 20, message = "min 2 and max 20 characters required")
 	private String name;
+
+	@Column(unique = true)
+	@Email(regexp = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$", message = "Invalid email")
 	private String email;
+
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Invalid password. It must be at least 8 characters long and contain at least one letter and one number.")
 	private String password;
-	
+
 	public User() {
 	}
-	
+
 	public User(int id, String name, String email, String password) {
 		super();
 		this.id = id;
@@ -28,11 +40,5 @@ public class User {
 		this.email = email;
 		this.password = password;
 	}
-
-
-
-	
-	
-	
 
 }
