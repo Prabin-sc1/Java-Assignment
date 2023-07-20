9a902c3bc245233dd218d792ea5205f2d89f4ee0
prabin.sc1 <prabin.sc1@gmail.com>
2023-07-20 20:08:52 +0545

    hello intern sathi, this is initial commit

---
+++ LoginRegisterAssignment/src/main/java/com/internsathi/assignment/LoginRegisterAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.internsathi.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisterAssignmentApplication.class, args);
+	}
+
+}
---
+++ LoginRegisterAssignment/src/test/java/com/internsathi/assignment/LoginRegisterAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internsathi.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
