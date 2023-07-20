465ae6221c5a5c2448e68acd235b923417a9bee2
prabin.sc1 <prabin.sc1@gmail.com>
2023-07-20 21:02:32 +0545

    model class for user created

---
+++ LoginRegisterAssignment/src/main/java/com/internsathi/assignment/model/User.java
@@ -0,0 +1,38 @@
+package com.internsathi.assignment.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	private String name;
+	private String email;
+	private String password;
+	
+	public User() {
+	}
+	
+	public User(int id, String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+
+
+	
+	
+	
+
+}
