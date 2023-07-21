53983a9a9df7b5434e75b3b37a6131bcf0342785
prabin.sc1 <prabin.sc1@gmail.com>
2023-07-21 16:50:52 +0545

    profile and change password module created

--- LoginRegisterAssignment/src/main/java/com/internsathi/assignment/controller/UserController.java
+++ LoginRegisterAssignment/src/main/java/com/internsathi/assignment/controller/UserController.java
@@ -1,15 +1,42 @@
 package com.internsathi.assignment.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.internsathi.assignment.dao.UserRepository;
+import com.internsathi.assignment.model.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	@Autowired
+	private UserRepository userRepository;
 
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String home() {
 		return "/user/home";
 	}
+
+	@RequestMapping("/profile")
+	public String profile(Model m, Principal p) {
+		String name = p.getName();
+		User tempUser = this.userRepository.getUserByUserName(name);
+		System.out.println(tempUser.getName());
+		m.addAttribute("title", "User profile");
+		m.addAttribute("user", tempUser);
+		return "user/profile";
+	}
+
+	@RequestMapping("/change-password")
+	public String changePassword(Model m) {
+
+		m.addAttribute("title", "Change password");
+
+		return "user/change_password";
+	}
+
 }
--- LoginRegisterAssignment/src/main/java/com/internsathi/assignment/dao/UserRepository.java
+++ LoginRegisterAssignment/src/main/java/com/internsathi/assignment/dao/UserRepository.java
@@ -1,6 +1,8 @@
 package com.internsathi.assignment.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.internsathi.assignment.model.User;
 
@@ -9,5 +11,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	boolean existsByEmail(String email);
 
 	User findByEmail(String email);
+
+	@Query("select u from User u where u.email = :email")
+	public User getUserByUserName(@Param("email") String email);
 
 }
