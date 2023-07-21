0ed913381cb2a5edd58770ead9036efa9a89afe2
prabin.sc1 <prabin.sc1@gmail.com>
2023-07-21 17:55:17 +0545

    Change Password module implemented

--- LoginRegisterAssignment/src/main/java/com/internsathi/assignment/controller/UserController.java
+++ LoginRegisterAssignment/src/main/java/com/internsathi/assignment/controller/UserController.java
@@ -3,18 +3,27 @@ package com.internsathi.assignment.controller;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.internsathi.assignment.dao.UserRepository;
+import com.internsathi.assignment.helper.MyMessage;
 import com.internsathi.assignment.model.User;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@RequestMapping("/")
 	public String home() {
@@ -39,4 +48,28 @@ public class UserController {
 		return "user/change_password";
 	}
 
+	@PostMapping("/changePassword")
+	public String changePassword(@RequestParam("oldPassword") String oldPassword,
+			@RequestParam("newPassword") String newPassword, Principal p, HttpSession session) {
+		System.out.println("Old password::" + oldPassword);
+		String name = p.getName();
+
+		User tempUser = this.userRepository.getUserByUserName(name);
+		String oldPassword1 = tempUser.getPassword();
+
+		if (this.bCryptPasswordEncoder.matches(oldPassword, oldPassword1)) {
+			tempUser.setPassword(this.bCryptPasswordEncoder.encode(newPassword));
+			this.userRepository.save(tempUser);
+
+			this.userRepository.save(tempUser);
+			session.setAttribute("msg", new MyMessage("Successfully Updated!! ", "alert-success"));
+			return "user/change_password";
+		} else {
+			System.out.println("password doesn't matches");
+			session.setAttribute("msg", new MyMessage("Password doesn't match. ", "alert-danger"));
+			return "user/change_password";
+
+		}
+
+	}
 }
