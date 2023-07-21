2793129307ae631d892090d64e112d51be458e4d
prabin.sc1 <prabin.sc1@gmail.com>
2023-07-21 17:57:43 +0545

    RestController created

---
+++ LoginRegisterAssignment/src/main/java/com/internsathi/assignment/controller/RestUserController.java
@@ -0,0 +1,11 @@
+package com.internsathi.assignment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestUserController {
+
+//	no idea what to do in this case
+// in assignment not mentioned clearly	
+
+}
